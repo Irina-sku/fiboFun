cedb6d7b42e378bd10b774d881bc40c2218ef92a
Irina-sku <irina.a.skuratova@gmail.com>
2020-01-21 22:10:14 +0100

    weird0 fibo

--- fibo.js
+++ fibo.js
@@ -1,7 +1,8 @@
 document.getElementById('run').addEventListener('click', function () {
-    //fibo();
+    fibo();
     fiboImproved();
     fiboBySearch();
+    toggleFibo();
 });
 
 function fibo() {
@@ -28,4 +29,24 @@ function fiboBySearch() {
           arr[i+2] = arr[i] + arr[i+1];
       }
       console.log(arr[userInput-1]);
+}
+
+function toggleFibo() {
+    let userInput = document.getElementById('input').value;
+    let arr = [0,1];
+    let bool = true;
+    for (var i = 0; i < userInput - 1; i++) {
+        if (bool === true) {
+            arr[0] += arr[1];
+            bool = false;
+        } else {
+            arr[1] += arr[0];
+            bool = true;
+        }
+    }
+    if (userInput%2 === 0) {
+        console.log(arr[1]);
+    } else {
+        console.log(arr[0]);
+    }
 }
