aa92b051c659f748f491f670629a6897e6cd19ef
Irina-sku <irina.a.skuratova@gmail.com>
2020-02-14 15:41:01 +0100

    added prime number generator

--- fibo.js
+++ fibo.js
@@ -1,39 +1,41 @@
-document.getElementById('run').addEventListener('click', function () {
-    fibo();
-    fiboImproved();
-    fiboBySearch();
-    toggleFibo();
-});
+//document.getElementById('run').addEventListener('click', function () {
+//fibo();
+fiboImproved();
+//fiboBySearch();
+//toggleFibo();
+console.log(primeNumbers());
+
+//});
 
 function fibo() {
-    let arr = [0,1];
-    for (var i = 0; 10; i++) {
-        let nxt = arr[i] + arr[i+1];
+    let arr = [0, 1];
+    for (var i = 0; i < 10; i++) {
+        let nxt = arr[i] + arr[i + 1];
         arr.push(nxt);
     }
     console.log(arr)
 }
 
 function fiboImproved() {
-    let arr = [0,1];
-    for (var i = 0; i < 10 ;i++) {
-        arr[i+2] = arr[i] + arr[i+1];
+    let arr = [0, 1];
+    for (var i = 0; i < 10; i++) {
+        arr[i + 2] = arr[i] + arr[i + 1];
     }
     console.log(arr);
 }
 
 function fiboBySearch() {
-      let userInput = document.getElementById('input').value;
-      let arr = [0,1];
-      for (var i = 0; i < userInput; i++) {
-          arr[i+2] = arr[i] + arr[i+1];
-      }
-      console.log(arr[userInput-1]);
+    let userInput = document.getElementById('input').value;
+    let arr = [0, 1];
+    for (var i = 0; i < userInput; i++) {
+        arr[i + 2] = arr[i] + arr[i + 1];
+    }
+    console.log(arr[userInput - 1]);
 }
 
 function toggleFibo() {
     let userInput = document.getElementById('input').value;
-    let arr = [0,1];
+    let arr = [0, 1];
     let bool = true;
     for (var i = 0; i < userInput - 1; i++) {
         if (bool === true) {
@@ -44,9 +46,29 @@ function toggleFibo() {
             bool = true;
         }
     }
-    if (userInput%2 === 0) {
+    if (userInput % 2 === 0) {
         console.log(arr[1]);
     } else {
         console.log(arr[0]);
     }
+}
+
+//PRIME NUMBERS
+function primeNumbers() {
+    let primeArray = [];
+    let isPrime = true;
+
+    for (let i = 1; i < 100; i++) {
+        for (let j = 2; j < i; j++) {
+            let rest = i % j;
+            if (rest === 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime === true) {
+            primeArray.push(i);
+        }
+        isPrime = true;
+    }
+    return primeArray;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 [Build the fibo](https://irina-sku.github.io/fiboFun/)
 
-Inspired by [EddyG](https://github.com/EddyGHendrickx) && [The Magic Xeno](https://github.com/Xenoxinius)
+Inspired by [EddyG](https://github.com/EddyGHendrickx) && [The Magic Xeno](https://github.com/Xenoxinius), [Hot&Spicy](https://www.github.com/Jensdn) && [Don Castillo](https://www.github.com/IvoCastillo)
